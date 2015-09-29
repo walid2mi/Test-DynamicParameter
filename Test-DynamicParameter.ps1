4080d15cc0ee5da9ef9fa461cff215a2bc0a577b
walid2mi <walidtoumi2020@gmail.com>
2015-09-28 22:18:52 -0700

    test dynamic parameter
    test dynamic parameter with ps v5

---
+++ Test-DynamicParameter.ps1
@@ -0,0 +1,49 @@
+﻿#requires -version 5
+
+using namespace system.management.automation
+
+function Test-DynamicParameter 
+{
+    [cmdletBinding()]
+    param(
+       [switch]$Remote
+    )
+
+    DynamicParam {
+
+    if($Remote.IsPresent)
+    {
+       $dict = [RuntimeDefinedParameterDictionary]::new()
+       $attr = [Collections.ObjectModel.Collection[Attribute]]::new()
+
+       $params = [parameter]::new()
+       $params.Mandatory = $true
+       $params.HelpMessage = 'Enter computer name or ip address'
+
+       $attr.Add($params)
+
+       $validValues = [ValidateSetAttribute]::new(
+           ((get-content "$home\desktop\computers.txt") -match '^\S' -split '\s+')
+       )
+       $attr.Add($validValues)
+       
+       $def = [RuntimeDefinedParameter]::new('ComputerName',[string],$attr)
+       $dict.Add('ComputerName', $def)  
+
+       # gps
+    
+       return $dict
+     }
+    }
+
+    End {
+       if($PSBoundParameters.computername)
+       {
+          "Remote Process...'$($PSBoundParameters.computername)'"
+       }
+       else
+       {
+          "Local Process..."
+       }
+    }
+}
